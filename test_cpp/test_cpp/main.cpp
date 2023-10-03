c2e325e6ea873c83f08ce47cfdbf47b3e129f59b
gyuhan <leegyuhan21@gmail.com>
2023-10-04 02:45:50 +0900

    4주차 Q26까지

---
+++ test_cpp/test_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test_cpp
+//
+//  Created by 이규한 on 2023/10/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

16a2deb2272aa45f2bdcded9f946b40486212d09
gyuhan <leegyuhan21@gmail.com>
2023-10-04 02:46:20 +0900

    Q26,test_cpp

--- test_cpp/test_cpp/main.cpp
+++ test_cpp/test_cpp/main.cpp
@@ -1,14 +1,52 @@
-//
-//  main.cpp
-//  test_cpp
-//
-//  Created by 이규한 on 2023/10/04.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main(){
+    vector<string> svec;
+
+    string input;
+    //string형의 input이 들어올때까지 무한히 입력을 받기 때문에 마지막 단어 입력 이후 엔터키를 입력하고 control + d 키를 이용해 EOF로 쉘 종료
+    while (cin>>input)
+        svec.push_back(input);
+    vector<int> countRepeat(svec.size(),1);
+
+    int maxCount;
+    string repeatdWord;
+    int cnt =1;
+
+    for(int i=0; i< svec.size()-1; i++){
+        string tempword = svec[i];
+        maxCount = *max_element(countRepeat.begin(),countRepeat.end());
+        if (i>0){
+            string prevWord = svec[i-1];
+            if(tempword != repeatdWord && tempword != prevWord){
+                for(int j=i+1; j < svec.size()-1-i; j++){
+                    if(tempword == svec[j])
+                        cnt++;
+                }
+            }
+            if(cnt != 1 && cnt > maxCount)
+                repeatdWord = tempword;
+        }
+        
+        else{
+            repeatdWord = tempword;
+            for(int j=i+1; j < svec.size()-1-i; j++){
+                if(tempword == svec[j])
+                    cnt++;
+            }
+        }
+        countRepeat[i] = cnt;
+        cnt = 1;
+    }
+    if (maxCount == 1)
+        cout << "No word was repeated";
+    else{
+        cout << "Most duplicated word : " << repeatdWord <<endl;
+        cout << "It occurred "<< maxCount << " times." << endl;
+    }
+
+
 }
--- 프입2/week4/Assignment3_Q26.cpp
+++ 프입2/week4/Assignment3_Q26.cpp
@@ -2,52 +2,38 @@
 #include <vector>
 using namespace std;
 
-
-int main(){
-    vector<string> svec;
-
+int main() {
+    vector<string> words;
     string input;
-    //string형의 input이 들어올때까지 무한히 입력을 받기 때문에 마지막 단어 입력 이후 엔터키를 입력하고 control + d 키를 이용해 EOF로 쉘 종료
-    while (cin>>input)
-        svec.push_back(input);
-    vector<int> countRepeat(svec.size(),1);
 
-    int maxCount;
-    string repeatdWord;
-    int cnt =1;
-
-    for(int i=0; i< svec.size()-1; i++){
-        string tempword = svec[i];
-
-        if (i>0){
-            string prevWord = svec[i-1];
-            if(tempword != repeatdWord && tempword != prevWord){
-                for(int j=i; j < svec.size()-1-i; j++){
-                    if(tempword == svec[j])
-                        cnt++;
-                }
-            }
-            if(cnt != 1 && cnt > maxCount)
-                repeatdWord = tempword;
-        }
-        
-        else{
-            repeatdWord = tempword;
-            for(int j=i+1; j < svec.size()-1-i; j++){
-                if(tempword == svec[j])
-                    cnt++;
-            }
-        }
-        countRepeat[i] = cnt;
-        maxCount = *max_element(countRepeat.begin(),countRepeat.end());
-        cnt = 1;
-    }
-    if (maxCount == 1)
-        cout << "No word was repeated";
-    else{
-        cout << "Most duplicated word : " << repeatdWord <<endl;
-        cout << "It occurred "<< maxCount << " times." << endl;
+    while (cin >> input) {
+        words.push_back(input);
     }
 
+    int maxCount = 1;
+    string repeatedWord;
 
+    for (int i=0; i<words.size(); i++) {
+        int count = 1;
+
+        for (int j=0; j<words.size(); j++) {
+            if (i != j && words[i] == words[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            repeatedWord = words[i];
+        }
+    }
+
+    if (maxCount ==1) {
+        cout << "No word was repeated." << endl;
+    } else {
+        cout << "Most duplicated word: " << repeatedWord << endl;
+        cout << "It occurred " << (maxCount) << " times." << endl;
+    }
 }
+
+

d1d751958d41e2631220387f7ecc949092d98c05
gyuhan <leegyuhan21@gmail.com>
2023-10-14 18:09:02 +0900

    프입 6주차 과제 완료

--- 프입2/week6/Assignment5_Q17.cpp
+++ 프입2/week6/Assignment5_Q17.cpp
@@ -47,6 +47,10 @@ int main(){
     cout << myScreen.get() << endl;
     //17번 두번째 소문제에 대한 답
 
-    //move, set 그리고 display 함수의 return type이 Screen&이 아닌 Screen 이라면 myScreen.move(4,0).set('#').display(cout); 같은 경우엔 move 및 set 변경값이 myScreen 객체의 복사본에 적용되어, return type이 Screen&일 경우와 똑같이 출력(display)되지만,
-    //myScreen.display(cout);같은 경우엔 myScreen.move(4,0).set('#').display(cout);에서의 move 및 set 값이 실제 객체에 적용된 것이 아니라 복사본에 적용된 것이기 때문에 처음에 설정한 Screen myScreen(5,5,'X'); 그대로 출력(display)된다.
+    /*
+    만약 `move`, `set`, 그리고 `display` 함수의 반환 type이 `Screen&` 대신 `Screen`일 경우, 
+    `myScreen.move(4,0).set('#').display(cout);` 경우엔 
+    `move` 및 `set` 함수는 `myScreen` 객체의 복사본에 대한 조작을 수행하며, 원본 객체 `myScreen`은 변경되지 않지만 display함수에 이용되는 복사된 객체가 변경되어 출력됨. 
+    하지만  `myScreen.display(cout);` 같은 경우엔 원래 생성된 `myScreen(5,5,'X');`와 동일한 내용을 출력함. 원본 객체인 원본 객채인 `myScreen`은 변경되지 않으며, 변경된 사항은 복사본에만 적용되기 때문.
+    */
 }

cebe88e09646f8706a1b90d57a4f2cdc129ebac2
gyuhan <leegyuhan21@gmail.com>
2023-11-07 12:04:28 +0900

    과제 주석 추가

--- 프입2/week9/Assignment7_Q10.cpp
+++ 프입2/week9/Assignment7_Q10.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 void isLongerThanSix(vector<string>&words){
+    //count_if 함수를 사용해 strin이 저장되어있는 벡터에서 sizerk 6 초과인 원소들을 가르키는 개수를 리턴받아 count에 저장
     auto count = count_if(words.begin(), words.end(),[](const string&a){return a.size()>6;});
     cout << "Container includes " << count << " words that greater than length 6" << endl;
 }
